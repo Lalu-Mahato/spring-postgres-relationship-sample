fe9a0c1aa4f3fe6b573defbf87ae52d9d3f55492
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2023-09-27 10:17:44 +0530

    Setup database

---
+++ src/main/java/com/example/springpostgresrelationshipsample/SpringPostgresRelationshipSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpostgresrelationshipsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresRelationshipSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgresRelationshipSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpostgresrelationshipsample/SpringPostgresRelationshipSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpostgresrelationshipsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresRelationshipSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
